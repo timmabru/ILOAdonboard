4432423485f1e28649dfa54bca15ba911776aec1
Mihai Abrudan <mihai.abrudan@flex.com>
2018-01-16 02:14:10 -0800

    [Studio] Modified screen "dashboard"

---
+++ client/controllers/app/dashboard.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_dashboard', app_dashboard);
+function app_dashboard($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -6,12 +6,12 @@ angular.module('app').config(function ($stateProvider) {
         templateProvider: function (app) {
             return app.templateProvider('app');
         }
-    }).state('app.blank', {
+    }).state('app.dashboard', {
         views: {
             app: {
-                controller: 'app_blank',
+                controller: 'app_dashboard',
                 templateProvider: function (app) {
-                    return app.templateProvider('app.blank');
+                    return app.templateProvider('app.dashboard');
                 }
             }
         }
